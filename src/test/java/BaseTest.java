819e82b6c0d27ff9328a400881eb3738492670c5
reggion89@mail.ru <121292Shar>
2023-02-21 13:37:53 +0500

    added 3 and 4 tests

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,4 +1,5 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -6,6 +7,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import java.util.concurrent.TimeUnit;
 
 public abstract class BaseTest {
+    Dimension widowSize = new Dimension(1800, 900);
     WebDriver driver;
 
     public void setUpHeadless() {
@@ -18,6 +20,8 @@ public abstract class BaseTest {
     public void setUp() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+        driver.manage().window().setSize(widowSize);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
     public void setUpFullScreen() {
--- src/test/java/TestSet.java
+++ src/test/java/TestSet.java
@@ -3,15 +3,21 @@ import org.apache.log4j.Logger;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.TimeUnit;
 
 public class TestSet extends BaseTest {
         private Logger logger = LogManager.getLogger(TestSet.class);
-    private String cookieButton = "#__next > div.sc-1l5foaq-0.gCyyUk.sc-1gmm0i0-0.bHUXbO > div.sc-1hmcglv-0.iiFycX > div > div > div > button > div";
+    private String cookieButton = "body > div.body-wrapper > div > div.container.cookies__container > div > div > button";
+    private String cookieButtonMain = "body > div.body-wrapper > div > div.container.cookies__container > div > div > button";
+    private static final String HIDE_COOKIE = "$('body > div.body-wrapper > div > div.container.cookies__container > div > div').hide();";
 
 
 
@@ -59,8 +65,84 @@ public class TestSet extends BaseTest {
 
     @Test
     public void test3() {
+        Logger log = LogManager.getLogger(LogTestClass.class);
+        setUp();
+//        Валидация дат предстоящих мероприятий:
+//        1 Пользователь переходит в раздел События -> Календарь мероприятий
+        driver.get("https://otus.ru/");
+
+        WebDriverWait clickMain = new WebDriverWait(driver, Duration.ofSeconds(10));
+        clickMain.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(cookieButtonMain)));
+//        driver.findElement(By.cssSelector(cookieButtonMain)).click();
+        ((JavascriptExecutor) driver).executeScript(HIDE_COOKIE);
+        // Нахождение кнопки принятия cookie-файлов и клик на нее
+        // Нахождение элемента меню "Обучение" и наведение на него курсора
+        WebElement educationMenu = driver.findElement(By.xpath("//span[text()='Обучение']"));
+        Actions actions = new Actions(driver);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOf(educationMenu));
+        actions.moveToElement(educationMenu).perform();
+
+        // Нахождение элемента "Календарь мероприятий" и переход на страницу
+        WebElement calendarLink = driver.findElement(By.cssSelector("body > div.body-wrapper > div > header > div > nav > div:nth-child(2) > div > div > div:nth-child(2) > div > a:nth-child(3)"));
+        calendarLink.click();
+        ((JavascriptExecutor) driver).executeScript(HIDE_COOKIE);
+
+//        2 На странице отображаются карточки предстоящих мероприятий.
+        WebElement eventElements = driver.findElement(By.className("dod_new-event"));
+
+        Assertions.assertTrue(eventElements.isDisplayed());
+        log.info("Найдены мероприятия");
+//        3 Даты проведения мероприятий больше или равны текущей дате
+        String eventRawDate = driver.findElement(By.xpath("/html/body/div[1]/div/div[1]/div/section/div[1]/a[1]/div/div[5]/div[1]/span[1]/span[2]")).getText()+" 2023";
+        log.info("Дата ближайшего мероприятия: "+ eventRawDate);
+        // Преобразуем текст даты в объект LocalDate
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMMM yyyy");
+        LocalDate eventDate = LocalDate.parse(eventRawDate, formatter);
+
+        // Получаем текущую дату
+        LocalDate currentDate = LocalDate.now();
+        if (eventDate.isBefore(currentDate)) {
+            log.error("Мероприятие прошло");
+        } else if (eventDate.isEqual(currentDate)) {
+            log.info("Мероприятие сегодня");
+        } else {
+            log.info("Мероприятие начнется"+ eventDate);
+        }
+        if (driver != null)
+            driver.close();
+}
 
 
+
+
+    @Test
+    public void test4(){
+        Logger log = LogManager.getLogger(LogTestClass.class);
+        setUp();
+        driver.get("https://otus.ru/");
+        WebDriverWait clickMain = new WebDriverWait(driver, Duration.ofSeconds(10));
+        clickMain.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(cookieButtonMain)));
+        ((JavascriptExecutor) driver).executeScript(HIDE_COOKIE);
+        // Нахождение элемента меню "Обучение" и наведение на него курсора
+        WebElement educationMenu = driver.findElement(By.xpath("//span[text()='Обучение']"));
+        Actions actions = new Actions(driver);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOf(educationMenu));
+        actions.moveToElement(educationMenu).perform();
+
+        // Нахождение элемента "Календарь мероприятий" и переход на страницу
+        WebElement calendarLink = driver.findElement(By.cssSelector("body > div.body-wrapper > div > header > div > nav > div:nth-child(2) > div > div > div:nth-child(2) > div > a:nth-child(3)"));
+        calendarLink.click();
+        ((JavascriptExecutor) driver).executeScript(HIDE_COOKIE);
+        WebElement eventElements = driver.findElement(By.className("dod_new-event"));
+        Assertions.assertTrue(eventElements.isDisplayed());
+        driver.findElement(By.cssSelector("body > div.body-wrapper > div > div.js-dod-new-events-root > div > section > header > div.dod_new-events__header-left > div > div.dod_new-events-dropdown__input > span")).click();
+        driver.findElement(By.xpath("//a[contains(text(), 'Открытый вебинар')]")).click();
+        String textFromEvent = driver.findElement(By.cssSelector("body > div.body-wrapper > div > div.js-dod-new-events-root > div > section > div.dod_new-events__list.js-dod_new_events > a:nth-child(1) > div > div.dod_new-event__type > div > div.dod_new-type__text")).getText();
+        Assertions.assertEquals(textFromEvent, "Открытый вебинар");
+        if (driver != null)
+            driver.close();
     }
 
 
