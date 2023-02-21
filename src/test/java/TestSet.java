8dc4e64d470479b816e9980471c39e62d255f7f1
reggion89@mail.ru <121292Shar>
2023-02-21 21:12:54 +0500

    fixed checkEventsDate method

--- src/test/java/TestSet.java
+++ src/test/java/TestSet.java
@@ -42,13 +42,13 @@ public class TestSet {
         new MainPage(driver).open();
         logger.info("Браузер открыт в максимальном разрешении.");
     }
-//    @AfterEach
-//    public void close(){
-//        if(driver != null){
-//            driver.close();
-//        driver.quit();}
-//        logger.info("Браузер закрыт после успешного теста");
-//    }
+    @AfterEach
+    public void close(){
+        if(driver != null){
+            driver.close();
+        driver.quit();}
+        logger.info("Браузер закрыт после успешного теста");
+    }
 
 
 
